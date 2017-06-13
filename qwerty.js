ff302f94c73aeb97098b296ab09fbf77c489b336
Vennders <vennders@mail.ru>
2017-06-13 21:21:14 +0300

    first time!

---
+++ qwerty.js
@@ -0,0 +1,83 @@
+$('document').ready(function() {
+
+$("img").addClass("bordik");
+	
+	
+	
+
+
+
+
+	
+	
+
+
+
+
+
+   
+     
+});
+
+ // $('#moto_models li').click(function(clelement) {
+ // 	var a = $(this).clone();
+ // 	$('#moto_table').after(a.fadeTo(1,0).fadeTo(1000,1));
+
+ 
+ // })
+
+
+
+
+	// // var textOrigin = $('#main_h1');
+	// // $('#knp').click(function() {
+	// // 	var textStartChange = $.when(
+
+	// // 	textOrigin.fadeTo(1000,0)
+	// // 	).then (function() {
+	// // 		textOrigin.text('is it change?');
+	// // 		textOrigin.fadeTo(1000,1);
+	// // 	})
+
+	// // 	$('#knp2').click(function() {
+	// // 		$.when(textOrigin.fadeTo(1000,0)).
+	// // 		then (function () {
+	// // 			textOrigin.text('Welcome to the MotoRent web-site!');
+	// // 		textOrigin.fadeTo(500,1);
+
+	// // 		})
+	// // 	});
+			
+				
+			
+		
+
+	// })
+	//меняе текст плавно на другий и обратно
+
+
+
+
+// $('#knp').click(function() {
+// 	var name = $('#vod').val();
+// 	var name2 = '#' + name;
+// 	var result = $(name2).width();
+// 	alert(result);
+
+// })
+//вычесляе ширину 
+
+
+
+// $("#knp").click(function() {
+// 	var name = $("#vod").val();
+// 	var name2 = '#' + name;
+// 	$(name2).fadeTo(2000,0);
+// })
+// Ищезае обект по имени яке введеш
+
+
+
+
+
+	
---
+++ test.js
@@ -0,0 +1,95 @@
+$(document).ready(function() {
+
+$("#pdtv").click(function Vubor () {
+	var bil = $("#chooseb").val();
+    if (bil == 1) {
+    	$("#nazva").text("1. Define translation as a notion")
+    	$("#samtext").text("Translation is the communication of the meaning of a source-language text by means of an equivalent target-language text")
+    } else if (bil == 2) {
+    	$("#nazva").text("2. Translation Studies as an academic discipline")
+    	$("#samtext").text("Translation studies is an academic interdiscipline dealing with the systematic study of the theory, description and application of translation, interpreting, and localization. As an interdiscipline, Translation Studies borrows much from the various fields of study that support translation. These include comparative literature, computer science, history, linguistics, philology, philosophy, semiotics, and terminology.")
+    } else if (bil == 3) {
+    	$("#nazva").text("3. Literal way of translating")
+    	$("#samtext").text("Literal translation, direct translation, or word-for-word translation is the rendering of text from one language to another one word at a time with or without conveying the sense of the original whole.")
+    }else if (bil == 4) {
+    	$("#nazva").text("4. Verbal way of translating")
+    	$("#samtext").text("Verbal translating conveys only the denotated meaning of the SL units and retains neither their orthographic nor sounding form.never say die-не падай духом")
+    }else if (bil == 5) {
+    	$("#nazva").text("5. Word-for-word way of translating")
+    	$("#samtext").text("Word-for-word way of translation /presents a consecutive verbal translation at the level of word-groups and subconsciously by students in the process of the translating alien grammatical constructions especially at the initial stage of learning a foreign language. Usually the students employ Word-for-word translation to convey a structure form, the order of words and the means of connections different from those in the TL. You are right to begin with.")
+    }else if (bil ==6 ) {
+    	$("#nazva").text("6. Interlinear translation")
+    	$("#samtext").text("Interlinear translation./is a strictly faithful rendering of some texts. It can be achieved through various transformation which are called forth by lexical, grammatical and stylistic divergences in languages. Interlinear translation do not convey the literal. While performing interlinear translation a translator tries to convey completely the content of a SL sentence, stanza or passage. The translating faithfully conveys not only the meaning of all notional word but also the context of different sense units, which have no structural eq-s in Ukr.")
+    }else if (bil ==7 ) {
+    	$("#nazva").text("7. Literary translation")
+    	$("#samtext").text("7 Literary translation. /represents the highest level of a translator's activity. It is always performed with may transformations which help to achieve the ease and beauty of original composition. The number of phrases and sentence in a literary translation is never the same means of expression or the number and quality of stylistic devices per paragraph. L. t.n convey the features of style and in great measure the genre peculiarities of the works under translation. Literary translation often requires linguistic historical and often inquiries in order to clarify obscure plane (historical events, notions of specific nations of colloquial, conversational dialectical and other kinds of lexical units often used by many authors in their literary works).")
+    }else if (bil ==8 ) {
+    	$("#nazva").text("8. Give the definition of a translation equivalent")
+    	$("#samtext").text("ways of translation. The process of translation involves simultaneously different aspects of the source language and those of the target language. These are morphology (word – building and word forms), the lexical ( words, phraseologisms , set expressions), syntax and stylistics. Literal translation is employed at word level when we deal with separate words whose surface from and structure ,as well as their lexical meaning in the source language and in the target language fully coincide.( administrator , region, hotel/motel). In many cases the lingual form of the source language words is only partly convoyed in the target language : anti alcoholic, music, zoology, national opera theater. Verbal translation is also employed at lexeme (fearful – страшний, fearless - безстрашний ). Verbal translation of polysemantic words permits a choice among some variants. The Ukrainian word «автомат» can have the following equivalent variants in English : 1) Automatic machine 2) Slot – machine 3) Automatic telephone 4) Submachine gun/tommy gun) English word “bank” 1. Берег (річки) 2. Бан 3. Вал / насип Verbal translation is employed only at a word level. When employed at the level of word-combinations or senteces verbal translation may often make the language units ungrammar. I am reading now. Never say die. To take measure. Word- for-word translation presents a consecutive verbal translation at the level of word – groups and sentences. You are right to begin with. Почнемо з того, що ти правий. The interlineal way/ method of translation (скорочений переклад / міжрядковий) is a strictly faithful rendering of sense expressed by word-groups and sentences. Literary translation represents the highest level of a transldtor’s activity. Literary translation convey the features of style and in great measure the genre peculiarities of the words under translation. Literary translation often requires linguistic , historical and other inquiries in order to clarify obscure place ")
+    }else if (bil == 9) {
+    	$("#nazva").text("9. Permanent translation equivalents")
+    	$("#samtext").text("translation English is similarity between a world or expression in one language and it’s translation in another .( A translation is a corresponding). Permanent translation (постійні перекладознавчі еквіваленти) 2 words are called permanent equivalent if their meanings exactly correspond and they may reciprocally substitute each other in any context ( geographical , proper names, terminology, some everyday speech). London, Monday, March Braggest – хвалько The amount of these lexems comprises about 30%.")
+    }else if (bil == 10) {
+    	$("#nazva").text("10. Non-permanent translations")
+    	$("#samtext").text("Non-permanent translations. /equivalency means that there is more than one meaning in the TL. The task is to choose the most appropriate meaning. In this case we have the one-to-many correspondence between SL and TL. Clandestine - 1) таємний, підпільний, прихований Clandestine marriage - таємний шлюб, Clandestine moment - прихований рух Non - permanent equivalents can be monosemantic and polysemantic. Monosemantic equivalents are synonymous word, and the test of the translator to choose the best correspondence. Дитина - child, toddler, kid. Polysemantic - have more than one meaning. Pen - 1) ручка, 2) стиль 3) стійло... In some cases the corresponding meanings of polysemantic words can be identified the already at word combination level : Advance of science - прогрес науки, Advance of army - наступ армії.")
+    }else if (bil == 11) {
+    	$("#nazva").text("11. Occasional translation equivalents")
+    	$("#samtext").text("Occasional translation equivalents /or contextual substitution is used when a word/word combination in the SL doesn't have an appointment equipment in the TL. In this case the translator creates an occasional equivalent or contextual substitution. The English word hostage the following meaning in the dictionary composed by Galperin : 1) заложник, 2) заклад (грошовий, майновий). Set phrase : hestage to fortune - дружина і діти.")
+    }else if (bil == 12 ) {
+    	$("#nazva").text("12. Define generalization as a translation transformation")
+    	$("#samtext").text(".Define generalization as a translation transformation is the conversion of a text in one language into a text in another language.As translation deals with language and presupposes a work in another language ,it demands a lingustistic scrutiny of the two languages and nature of relations between their stylistic means.")
+    }else if (bil == 13) {
+    	$("#nazva").text("13. Define concretization as a translation transformation")
+    	$("#samtext").text(".concretization —can be defined as replacement of a lexical unit of broad general meaning with a lexical unit of narrowed more specific meaning. There is a large group of words in the English vocabularity of wide semantic volume that are smth necessary diferense in translation For example : the focus in the description of process - основну увагу слід приділити опису процесу")
+    }else if (bil == 14) {
+    	$("#nazva").text("14. Decompression of words")
+    	$("#samtext").text("decomprassion or addition of words is the opposite process to compression,the increase in the volume of the text in TL as compared to SL text.When translating from Ukrainian into English decompression among many other things,includes addition of possesive and other pronouns.")
+    }else if (bil == 15 ) {
+    	$("#nazva").text("15. Compression of words")
+    	$("#samtext").text("------------")
+    }else if (bil == 16) {
+    	$("#nazva").text("16. Antonymous translation")
+    	$("#samtext").text("Antonymous translation is a lexico-grammatical device which consists in replacing an affirmative phrase in the source language sentence by a negative one in the target language sentence, and vice versa. This transformation is accompanied by replacing one of the words in the original sentence by its antonym in the resulting sentence.")
+    }else if (bil == 17) {
+    	$("#nazva").text("17. Descriptive translation")
+    	$("#samtext").text("Descriptive Translating of International Lexemes.There are semantically condensed and can be transl. into the targer language only in a descriptive way. civilizable той, хто піддається цивілізації, examinee той хто екзаменується, barony володіння барона")
+    }else if (bil == 18) {
+    	$("#nazva").text("18. Grammatical translation transformation")
+    	$("#samtext").text("Grammatical transformation Changes in the word orderThe English language is characterized by a stable word order any changes in it brings about changes in the meaning of a sentence. We have to keep in mind the following things:1) in the process of translation we mast follow exactly the word order of the English sentence 2) the logical in the sentences occupy different places:in English - out of beginning; Ukraine - of the endPartitioning is a translation devise when one SL sentence is transformed into two or more TL sentence.Integration is transl.device that consist in combining two or more source language sentence into target sentence or compressing a complex sentence into simple one.Replacements.It can be the replacements of one gram category with another gram category in the prouss of translation.We have to replace a plural noun in English with a singular noun in Ukraine. Australian prosperity was followed by a slump - за розквітом Австралії прийшла криза")
+    }else if (bil == 19) {
+    	$("#nazva").text("19. Transcoding")
+    	$("#samtext").text("Transcoding- technique when words are translation in the true sence of the word but turned into TL in their phonetic or orthographic form/ struct. Peak- пік, London -Лондон, organism- організм")
+    }else if (bil == 20) {
+    	$("#nazva").text("20. Types of transcoding")
+    	$("#samtext").text("Types of transcoding.Transcribing- used when the SL sounding form is conveyed by means of TL letters.Transliteration-used when the SL orthographic form is conveyed means of the TL letters Mixed transcoding -use of transcribing with elements of transliteration Adapted transcoding-used when the form of the SL word is partly adapted to phonetic or morphorogical structure of the TL (idea-ідея)")
+    }else if (bil == 21) {
+    	$("#nazva").text("21. Methods and ways of translating Ukrainian proper names")
+    	$("#samtext").text("-------------------")
+    }else if (bil == 22) {
+    	$("#nazva").text("22. Methods and ways of translating English proper names")
+    	$("#samtext").text("---------------")
+    }else if (bil == 23) {
+    	$("#nazva").text("23. Geographical names. Methods and ways of translating")
+    	$("#samtext").text("The geographical names formed on the basic of common nouns which acquired the status of proper names are generally translated from English into Ukraine and vice versa: Cape of Good Hope- мис Доброї Надії, Golden Gate - золоті ворота.Some geographical names that have single word and word combination structures require some identifying element in the target language (an appositional noun). These elements explain the nature and real meaning of the geographical name in the target language: the Azores(the Seychelles)- азорські (сейшелеські о-ви) , the Saint Lawrence River - ріка св.лаврентія")
+    }else if (bil == 24) {
+    	$("#nazva").text("24. Ways of translating genuine internationalisms")
+    	$("#samtext").text("")
+    }else if (bil == 25) {
+    	$("#nazva").text("25. Pseudo-internationalisms")
+    	$("#samtext").text("Pseudo-internationalisms are words which have identical orthographic forms but quite different lexical meanings and often referred to as false friends of the translator: clay- глина, compositor-викладач, corpse-труп, data-дата")
+    }else if (bil == 26) {
+    	$("#nazva").text("26. Free word combinations and idiomatic expressions")
+    	$("#samtext").text("Free word combinations and idiomatic expressions. 1-are expression translated at the word level and the meaning of its a sum of the meanings of these components. To write a letter. Idiomatic or phraseological expressions are structurally, lexically, semantically fixed phrases which mostly have the meaning, which isn't made up by the sum of meaning of their componental parts. An indispensable features of idiomatic expressions is their figurative ( methaforical nature and asage). Some Eng. words combi-s or phrases can function both as free or idiomatic word combi-ns ( it depends on contextual environment). In translation there are 5 important factor : 1)similarity or difference between sources if origin 2) one or more than one or all componental images 3) componental images, when translated are perceived by the TL speakers 4) structural forms ( it can be maintained in the TL) 5)existence of analogues in sense idiom. expr-s in the TL ")
+    }else if (bil == 27) {
+    	$("#nazva").text("27. Ways of translating idiomatic expressions")
+    	$("#samtext").text("Ways of translating idiomatic expressions.1) By choosing Absolute / Complete equivalents The method by which every component of the SL idiom is related in the TL unchanged ( when they originate from the same source): mythology, ancient history or literature, the Bible, one source of different work of literature, expressions belonged to prominent English and American authors (time is money-час-гроші, the golden age - золотий вік). 2) By choosing near equivalents. There are one or even most of components different. Cut once - сім раз відмірь, один раз відріж.The slight divergence in the near equivalents as compared the SL idioms can manifest themselves in different aspects: in the structure,in the omission / addition, the substitution, the generalization, the concretization. 3) By choosing genuine idiomatic analogies. When we can easily give corresponding analogies in the TL. Such expressions are something very close in their connotative meaning. Sometimes there two or more analogies (the choice depends on the style of the text). Not for love or money - Не за що на світі / скарби в світі.4) By choosing approximate analogies. In such an expression there can be part which can be understood by native speakers only. As a result, there isn't any genuine form, and we can give only approximately analogy. 5) Descriptive translation. We are able to describe the meaning of the idiomatic expressions. The are ways : 1) By a single word (poor fish - нікчема), 2) word combinations ( to go wrack - зовсім розкрилися), 3) sentence ( idiomatic expression is very specific, for example, because of mentality) (a wet blanket - людина/обставина, що розхолоджує.")
+    }
+});
+
+
+
+	
+
+
+	
+});
